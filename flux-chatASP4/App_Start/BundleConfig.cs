b2010f119eebf85dbd6019f0f5e7cf76a4c1bfda
gunnarsireus <gunnar.sireus@gmail.com>
2016-08-22 16:57:11 +0200

    Add project files.

---
+++ flux-chatASP4/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+using System.Web.Optimization.React;
+
+namespace ReactDemo
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new BabelBundle("~/bundles/main").Include(
+                "~/flux-chat/js/bundle.js"
+                ));
+
+            // Forces files to be combined and minified in debug mode
+            // Only used here to demonstrate how combination/minification works
+            // Normally you would use unminified versions in debug mode.
+            BundleTable.EnableOptimizations = false;
+        }
+    }
+}
